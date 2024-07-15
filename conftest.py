e6d4a51bb989e6a9d690c65ceb691c2e20819a2b
Ainur <ainur.kurbanov.94@mail.ru>
2024-07-15 13:41:25 +0300

    1)  добавил опцию для вебдрайвера

--- conftest.py
+++ conftest.py
@@ -10,6 +10,7 @@ def driver(request):
     options.add_argument("start-maximized")
     options.add_argument("—disable-notifications")
     options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--windows-size=1920,1080")
     driver = webdriver.Chrome(options=options)
